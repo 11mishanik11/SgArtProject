ccd04b1d443b248beba11b27183d8de9e3135c45
Mishail <93082995+11mishanik11@users.noreply.github.com>
2023-04-30 14:01:13 +0300

    finaly build

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3037,6 +3037,44 @@ function headerFixed() {
 }
 
 
+// бургер меню
+const burgerBtn = document.querySelector('#burger-btn')
+const headerMenu = document.querySelector('#header-menu')
+
+
+bodyPage.onresize = function() {
+    if(window.innerWidth < 993) {
+        burgerBtn.setAttribute('aria-expanded', 'false')
+        headerMenu.setAttribute('aria-hidden', 'true')
+    } else {
+        burgerBtn.removeAttribute('aria-expanded')
+        headerMenu.removeAttribute('aria-hidden')
+    }
+}
+
+
+burgerBtn.onclick = function() {
+    if(headerMenu.classList.contains('open')) {
+        menuHide()
+    }
+    else {
+        menuShow()
+    }
+}
+
+function menuShow() {
+    headerMenu.classList.add('open')
+    burgerBtn.setAttribute('aria-expanded', 'true')
+    headerMenu.setAttribute('aria-hidden', 'false')
+}
+
+function menuHide() {
+    headerMenu.classList.remove('open')
+    burgerBtn.setAttribute('aria-expanded', 'false')
+    headerMenu.setAttribute('aria-hidden', 'true')
+}
+
+
 
 /*
 В аргумент передается весь блок с аккордеоном. По умолчанию '[data-accordeon]'
